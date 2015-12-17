636c71f517f220c22df70e43b9db70be7bcbefe0
Jens Hausherr <jens.hausherr@xing.com>
2015-12-17 13:21:36 +0100

    Impl challenge 6

--- 05.rb
+++ 05.rb
@@ -5,5 +5,4 @@ c1 = list.reject{ |e| e == nil || e.strip.length == 0 }.reject{ |e| /.*?ab|cd|pq
 c2 = c1.reject{ |e| e.gsub(/[^aeiou]/, '').length<3 }
 puts c2.count{ |e| /(.)\1/ =~ e }
 
-
 puts list.select{ |e| /(..).*?\1/ =~ e }.count{ |e| /(.).\1/ =~ e}
--- 06.rb
+++ 06.rb
@@ -5,7 +5,7 @@ class LightArray
   INSTRUCTION = /^(?<instr>turn o(n|ff)|toggle) (?<X0>\d{1,3}),(?<Y0>\d{1,3}) through (?<X1>\d{1,3}),(?<Y1>\d{1,3})$/
 
   def initialize(x,y)
-    @lights= Array.new(x,Array.new(y,0))
+    @lights=Array.new(x){ |i| Array.new(y,0)}
   end
 
   def parse(line)
@@ -20,12 +20,26 @@ class LightArray
         when :'turn off'
           @lights[x][y]=0
         when :'turn on'
-          @lights[x][y]=0
+          @lights[x][y]=1
         when :toggle
           old = @lights[x][y]
-          @lights[x][y] = (@lights[x][y]-1).abs
-        else
-          puts "Unknown instruction: #{command}"
+          @lights[x][y] = (old-1).abs
+        end
+      end
+    end
+  end
+
+  def brightness(command, x0,y0,x1,y1)
+    (x0..x1).each do |x|
+      (y0..y1).each do |y|
+        case command
+        when :'turn off'
+          bn = @lights[x][y]-1
+          @lights[x][y]=(bn < 0 ? 0 : bn)
+        when :'turn on'
+          @lights[x][y]+=1
+        when :toggle
+          @lights[x][y]+=2
         end
       end
     end
@@ -33,10 +47,18 @@ class LightArray
 
   def light_it_up
     IO.readlines("06-input.txt").each { |i| execute_step(*parse(i)) }
-    i=0
-    puts @lights.reduce(0){ |memo,e| l= e.reduce(:+); puts "#{i}:\t#{l}"; i+=1; memo + l }
+    on= @lights.reduce(0){ |memo,e| memo + e.reduce(:+) }
+    off= @lights.reduce(0){ |memo,e| memo + e.count{ |l| l == 0 } }
+    puts "ON: #{on}, OFF: #{off} (#{on+off})"
   end
 
+  def brighten
+    IO.readlines("06-input.txt").each { |i| brightness(*parse(i)) }
+    on= @lights.reduce(0){ |memo,e| memo + e.reduce(:+) }
+    off= @lights.reduce(0){ |memo,e| memo + e.count{ |l| l == 0 } }
+    puts "total brightness: #{on}, OFF: #{off} (#{on+off})"
+  end
 end
 
-LightArray.new(1000,1000).light_it_up
+#LightArray.new(1000,1000).light_it_up
+LightArray.new(1000,1000).brighten
